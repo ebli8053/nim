d168d93fb8c6773ef8709f35a3756de836ba3365
Ludvig Hult <ludvig.hult@gmail.com>
2023-01-25 14:23:07 +0100

    Fixed broken lint rule and test rules

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  collectCoverage: true,
-  testPathIgnorePatterns: ['./web/script'],
+  testPathIgnorePatterns: ["./build"],
 };
